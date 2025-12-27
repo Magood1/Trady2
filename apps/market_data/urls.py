77cb7601806e958e39d35ce8a5130c63dc6fbbb5
= <magdhndi40@gmail.com>
2025-12-27 23:38:52 +0300

    chore: clear index to apply gitignore rules

--- apps/market_data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import OHLCVIngestAPIView
-
-# هذه القائمة هي ما يبحث عنه Django
-urlpatterns = [
-    path("ohlcv/ingest/", OHLCVIngestAPIView.as_view(), name="ohlcv_ingest"),
-]
--- project_trady2/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_trady2.settings")
-app = Celery("project_trady2")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
-
